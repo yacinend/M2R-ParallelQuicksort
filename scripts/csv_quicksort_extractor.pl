f77a4851c20f59b5e505e06c042b923e07a14d03
yacinend <yacinenana@gmail.com>
2016-01-31 20:08:07 +0100

    maj des fichiers

--- scripts/csv_quicksort_extractor.pl
+++ scripts/csv_quicksort_extractor.pl
@@ -3,16 +3,21 @@ use strict;
 
 my($line);
 my($size);
+my($compil);
 
-print "Size, Type, Time\n" ;
+print "Size, Compilation, Type, Time\n" ;
 while($line=<>) {
     chomp $line;
     if($line =~/^Size: ([\d\.]*)$/) {
         $size = $1;
         next;
     } 
+     if($line =~/^Compilation: (.*)/) {
+        $compil = $1;
+        next;
+    }
     if($line =~/^(.*) quicksort.*: ([\d\.]*) sec.$/) {
-        print "$size, \"$1\", $2\n" ;
+        print "$size, \"$compil\", \"$1\", $2\n" ;
         next;
     } 
 }
--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -4,9 +4,14 @@ mkdir -p $OUTPUT_DIRECTORY
 OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
 
 touch $OUTPUT_FILE
-for i in 100 1000 10000 100000 1000000; do
-    for rep in `seq 1 5`; do
-        echo "Size: $i" >> $OUTPUT_FILE;
-        ./src/parallelQuicksort $i >> $OUTPUT_FILE;
+for i in  1000 2500000 10000 100 5000 1000000 800 100000 430 5000000 4000; do
+    for rep in `seq 1 10`; do
+	for j in "-O" "-O2" "-O3"; do
+		echo "Size: $i" >> $OUTPUT_FILE;
+		echo "Compilation: $j" >> $OUTPUT_FILE;
+		touch ./src/parallelQuicksort.c;
+		make CFLAGS+=$j -C ./src/;
+		./src/parallelQuicksort $i >> $OUTPUT_FILE;
+	done;
     done ;
 done
