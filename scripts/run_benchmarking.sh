d52655049eac11f119c68e8d3e77a7fa87ce3793
yacinend <yacinenana@gmail.com>
2016-02-01 11:26:37 +0100

    update f the script file, rmd and exprimetns output

--- scripts/run_benchmarking.sh
+++ scripts/run_benchmarking.sh
@@ -6,11 +6,11 @@ OUTPUT_FILE=$OUTPUT_DIRECTORY/measurements_`date +%R`.txt
 touch $OUTPUT_FILE
 for i in  1000 2500000 10000 100 5000 1000000 800 100000 430 5000000 4000; do
     for rep in `seq 1 10`; do
-	for j in "-O" "-O2" "-O3"; do
+	for j in "-O1" "-O2" "-O3"; do
 		echo "Size: $i" >> $OUTPUT_FILE;
 		echo "Compilation: $j" >> $OUTPUT_FILE;
 		touch ./src/parallelQuicksort.c;
-		make CFLAGS+=$j -C ./src/;
+		make CFLAGS=$j -C ./src/;
 		./src/parallelQuicksort $i >> $OUTPUT_FILE;
 	done;
     done ;
